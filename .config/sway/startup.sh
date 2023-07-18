042870869f8cbc3e77dd05128bb115267ddbe674
Kenny Lam <2017lamk@gmail.com>
2023-07-18 16:54:05 -0400

    Revise Sway startup
    The revision to the Sway startup configuration made in Commit 82a4fcb
    (Revise Sway startup, 2023-06-22) does not open the preferred web
    browser in the intended workspace. This issue occurred because the web
    browser does not initialize before the subsequent switch-workspace
    command is executed.
    
    This commit corrects this problem by adding a delay before switching
    workspaces. To implement this delay, the web browser initialization is
    moved to startup.sh, where the execution path is defined (compared with
    Sway "exec" commands, which execute asynchronously). For this reason,
    this commit also extracts the delays embedded in Sway "exec" commands in
    startup.sh. This change makes adding delays between such commands easier
    since each delay no longer depends on the previous one. Finally, this
    commit passes commands that do not contain Sway variables to the shell
    instead of Sway (via swaymsg).
    
    Inline comments are edited to reflect these changes.

--- .config/sway/startup.sh
+++ .config/sway/startup.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/bash
 
+swaymsg 'workspace 2'
+
+swaymsg 'exec $browser'
+# Let web browser finish initializing.
+sleep 5
+
 swaymsg 'workspace 1'
 
-# Start tmux; $term_options is set by `kanshi`
+# Restart tmux server
+tmux kill-server
+# Start tmux; term_options is set by `kanshi`
 swaymsg 'exec $term $term_options -e tmux'
 
 ### Open an ASCII table in the scratchpad for reference ###
 # Delay opening this terminal window until the initial terminal window is
 # opened to guarantee the ordering of `tmux` sessions.
-swaymsg 'exec "sleep 1; $term $term_options -e tmux new-session -sref"'
-# Wait a couple of seconds so that the new `tmux` session completes
-# initialization before sending keys to it. (Add one second because these
-# "exec" commands execute simultaneously.)
-swaymsg 'exec "sleep 3; tmux send-keys -tref source Space ~/.scratchpad/.zshrc Enter"'
+sleep 1
+swaymsg 'exec "$term $term_options -e tmux new-session -sref"'
+# Wait until the new `tmux` session completes initialization before sending
+# keys to it.
+sleep 1
+tmux send-keys -tref source Space ~/.scratchpad/.zshrc Enter
