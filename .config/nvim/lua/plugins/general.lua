fd52fd64dc3990f3556eeea0e566725e8e69c32b
Kenny Lam <kenny@laminosity.com>
2024-02-20 17:04:42 -0500

    Format comments according to 'textwidth'

--- .config/nvim/lua/plugins/general.lua
+++ .config/nvim/lua/plugins/general.lua
@@ -11,7 +11,8 @@ return {
       {
         'nvim-telescope/telescope-fzf-native.nvim',
         -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
+        --       refer to the README for telescope-fzf-native for more
+        --       instructions.
         build = 'make',
         cond = function()
           return vim.fn.executable 'make' == 1
