8b5e96f0b6af6b0605fc7b931b2607008041523c
Kenny Lam <kenny@laminosity.com>
2024-03-14 11:57:00 -0400

    Update hrsh7th/nvim-cmp config
    This commit updates the plugin specification for hrsh7th/nvim-cmp. It
    adds the hrsh7th/cmp-path source, removes unused mappings, modifies
    existing mappings, and amends the plugin's configuration.

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,10 +1,12 @@
 return {
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
     dependencies = {
+      'L3MON4D3/LuaSnip',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-nvim-lsp',
-      'L3MON4D3/LuaSnip',
+      'hrsh7th/cmp-path',
       'saadparwaiz1/cmp_luasnip',
     },
     config = function()
@@ -12,7 +14,6 @@ return {
       -- See `:help cmp`
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
-      require('luasnip.loaders.from_vscode').lazy_load()
       luasnip.config.setup {}
 
       cmp.setup {
@@ -26,11 +27,8 @@ return {
           ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete {},
-          ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          },
+          ['<C-y>'] = cmp.mapping.confirm { select = true },
+
           ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
@@ -53,9 +51,12 @@ return {
         sources = {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
+          { name = 'path' },
           { name = 'buffer', keyword_length = 5 },
         },
         formatting = {
+          expandable_indicator = true,
+          fields = { "abbr", "kind", "menu" },
           format = function(entry, vim_item)
             vim_item.menu = ({
               buffer = "[buf]",
@@ -63,7 +64,7 @@ return {
               luasnip = "[snip]",
             })[entry.source.name]
             return vim_item
-          end
+          end,
         },
       }
     end
