f7dc9a9e442f89078ecb753094157f31dff273c2
Kenny Lam <kenny@laminosity.com>
2024-03-15 12:36:46 -0400

    Change colors of search highlight groups
    This commit changes the colors of the search highlight groups to make
    occurrences of a search pattern more visible.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,16 @@ return {
     'shaunsingh/solarized.nvim',
     priority = 1000,
     config = function()
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        group = vim.api.nvim_create_augroup('highlight-colorscheme', { clear = true }),
+        pattern = 'solarized',
+        callback = function()
+          vim.cmd.highlight('clear IncSearch')
+          vim.cmd.highlight('clear Search')
+          vim.cmd.highlight('IncSearch cterm=standout gui=standout guifg=#cb4b16')
+          vim.cmd.highlight('Search cterm=reverse gui=reverse guifg=#b58900')
+        end,
+      })
       vim.cmd.colorscheme 'solarized'
     end,
   },
