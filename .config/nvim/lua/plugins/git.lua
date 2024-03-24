ad9d9919891d57f269293c7cd0da154332ea5e9e
Kenny Lam <kenny@laminosity.com>
2024-03-24 11:16:47 -0400

    Move lewis6991/gitsigns.nvim to git.lua
    Although lewis6991/gitsigns.nvim makes some UI changes, it is more
    related to Git plugins than UI ones.

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -24,6 +24,35 @@ return {
       end, '[L]og --[p]retty')
     end
   },
+
+  { -- Adds git related signs to the gutter, as well as utilities for managing
+    -- changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr)
+        local gs = require('gitsigns')
+        local function nbmap(lhs, rhs, opts)
+          opts.buffer = bufnr
+          vim.keymap.set('n', lhs, rhs, opts)
+        end
+        nbmap('<Leader>hb', gs.blame_line, { desc = '`gitsigns` blame line' })
+        nbmap('<Leader>hn', gs.next_hunk, { desc = '`gitsigns` next hunk' })
+        nbmap('<Leader>hp', gs.prev_hunk, { desc = '`gitsigns` prev hunk' })
+        nbmap('<Leader>hv', gs.preview_hunk, { desc = '`gitsigns` preview hunk' })
+        nbmap('<Leader>hr', gs.reset_hunk, { desc = '`gitsigns` reset hunk' })
+        nbmap('<Leader>hs', gs.stage_hunk, { desc = '`gitsigns` stage hunk' })
+        nbmap('<Leader>hu', gs.undo_stage_hunk, { desc = '`gitsigns` undo stage hunk' })
+      end
+    },
+  },
 }
 
 -- vim:sw=2:et:
