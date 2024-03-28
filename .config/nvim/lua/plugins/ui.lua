2297795e79ecfa69ba3fe69f7fe61db0a41dbeb4
Kenny Lam <kenny@laminosity.com>
2024-03-28 10:29:56 -0400

    Add nvim-tree/nvim-web-devicons plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -20,6 +20,11 @@ return {
       signs = false,
     },
   },
+
+  { -- Add pretty icons to plugins supporting them
+    'nvim-tree/nvim-web-devicons',
+    enabled = vim.g.have_nerd_font, -- requires a Nerd Font
+  },
 }
 
 -- vim:sw=2:et:
