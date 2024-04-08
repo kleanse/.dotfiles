dfe3213f0053b6e047f8bb1c71427ba848a05928
Kenny Lam <kenny@laminosity.com>
2024-04-08 11:12:26 -0400

    Uppercase mode name displayed by mini.statusline

--- .config/nvim/lua/plugins/general.lua
+++ .config/nvim/lua/plugins/general.lua
@@ -42,7 +42,30 @@ return {
       -- Evaluate, exchange, multiply, replace, and sort text
       require('mini.operators').setup()
 
-      require('mini.statusline').setup()
+      require('mini.statusline').setup {
+        content = {
+          active = function()
+            local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+            local git           = MiniStatusline.section_git({ trunc_width = 75 })
+            local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+            local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+            local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+            local location      = MiniStatusline.section_location({ trunc_width = 75 })
+            local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+            mode = string.upper(mode)
+
+            return MiniStatusline.combine_groups({
+              { hl = mode_hl,                  strings = { mode } },
+              { hl = 'MiniStatuslineDevinfo',  strings = { git, diagnostics } },
+              '%<', -- Mark general truncate point
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl,                  strings = { search, location } },
+            })
+          end
+        }
+      }
 
       -- Statusline highlight groups
       vim.api.nvim_create_autocmd('ColorScheme', {
