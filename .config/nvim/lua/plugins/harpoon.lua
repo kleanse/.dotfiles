3037b84ad5ad83a6e3777e4cfb105de32f579562
Kenny Lam <kenny@laminosity.com>
2024-04-10 19:24:17 -0400

    Enable save_on_toggle for ThePrimeagen/harpoon
    Preserve the quick-menu behavior of Harpoon 1, i.e., save the state of
    the Harpoon list when toggling quick menu.

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -7,7 +7,11 @@ return {
     },
     config = function()
       local harpoon = require 'harpoon'
-      harpoon:setup()
+      harpoon:setup {
+        settings = {
+          save_on_toggle = true,
+        },
+      }
 
       local nmap = function(keys, func, desc)
         desc = desc and 'Harpoon: ' .. desc
