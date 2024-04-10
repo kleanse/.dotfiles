bdd0a6b8053e3a9673b246e2b9fc35380cd6793f
Kenny Lam <kenny@laminosity.com>
2024-04-10 16:24:30 -0400

    Update ThePrimeagen/harpoon to Harpoon 2

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,20 +1,32 @@
 return {
   { -- Harpoon: rapid file navigation
     'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
     config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+
       local nmap = function(keys, func, desc)
         desc = desc and 'Harpoon: ' .. desc
         vim.keymap.set('n', keys, func, { desc = desc })
       end
-      local mark = require('harpoon.mark')
-      local ui = require('harpoon.ui')
 
-      nmap('<M-m>', function() mark.add_file() end, 'mark current file')
-      nmap('<M-l>', function() ui.toggle_quick_menu() end, 'toggle quick menu')
-      nmap('<M-h>', function() ui.nav_file(1) end, 'edit file 1')
-      nmap('<M-t>', function() ui.nav_file(2) end, 'edit file 2')
-      nmap('<M-n>', function() ui.nav_file(3) end, 'edit file 3')
-      nmap('<M-s>', function() ui.nav_file(4) end, 'edit file 4')
+      nmap('<M-m>', function() harpoon:list():add() end, 'mark current file')
+      nmap('<M-h>', function() harpoon:list():select(1) end, 'edit file 1')
+      nmap('<M-t>', function() harpoon:list():select(2) end, 'edit file 2')
+      nmap('<M-n>', function() harpoon:list():select(3) end, 'edit file 3')
+      nmap('<M-s>', function() harpoon:list():select(4) end, 'edit file 4')
+
+      local toggle_opts = {
+        border = 'rounded',
+        title_pos = 'center',
+      }
+      nmap('<M-l>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list(), toggle_opts)
+      end, 'toggle quick menu')
     end
   },
 }
