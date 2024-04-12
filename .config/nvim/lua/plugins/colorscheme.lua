0e5985db76caa79d7976f2de1da5444b932c8544
Kenny Lam <kenny@laminosity.com>
2024-04-12 10:35:22 -0400

    Align mini.statusline highlight group attributes
    To make the attributes easier to compare with each other.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,12 +8,12 @@ return {
         pattern = 'gruvbox',
         callback = function()
           -- mini.statusline highlight groups
-          vim.cmd.highlight('MiniStatuslineModeNormal gui=bold guifg=#fbf1c7 guibg=#928374')
-          vim.cmd.highlight('MiniStatuslineModeInsert gui=bold guifg=#fbf1c7 guibg=#076678')
-          vim.cmd.highlight('MiniStatuslineModeVisual gui=bold guifg=#fbf1c7 guibg=#8f3f71')
+          vim.cmd.highlight('MiniStatuslineModeNormal  gui=bold guifg=#fbf1c7 guibg=#928374')
+          vim.cmd.highlight('MiniStatuslineModeInsert  gui=bold guifg=#fbf1c7 guibg=#076678')
+          vim.cmd.highlight('MiniStatuslineModeVisual  gui=bold guifg=#fbf1c7 guibg=#8f3f71')
           vim.cmd.highlight('MiniStatuslineModeReplace gui=bold guifg=#fbf1c7 guibg=#9d0006')
           vim.cmd.highlight('MiniStatuslineModeCommand gui=bold guifg=#fbf1c7 guibg=#b57614')
-          vim.cmd.highlight('MiniStatuslineModeOther gui=bold guifg=#fbf1c7 guibg=#79740e')
+          vim.cmd.highlight('MiniStatuslineModeOther   gui=bold guifg=#fbf1c7 guibg=#79740e')
         end,
       })
 
