f9efa8ece8533878bc74d5d63d9f5fb0a572d473
Kenny Lam <kenny@laminosity.com>
2024-04-12 10:40:16 -0400

    Add fg colors to GruvboxUnderline highlight groups

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,18 @@ return {
     'ellisonleao/gruvbox.nvim',
     priority = 1000,
     config = function()
+      require('gruvbox').setup {
+        overrides = {
+          GruvboxAquaUnderline   = { fg='#427b58' },
+          GruvboxBlueUnderline   = { fg='#076678' },
+          GruvboxGreenUnderline  = { fg='#79740e' },
+          GruvboxOrangeUnderline = { fg='#af3a03' },
+          GruvboxPurpleUnderline = { fg='#8f3f71' },
+          GruvboxRedUnderline    = { fg='#9d0006' },
+          GruvboxYellowUnderline = { fg='#b57614' },
+        },
+      }
+
       vim.api.nvim_create_autocmd('ColorScheme', {
         group = vim.api.nvim_create_augroup('highlight-colorscheme', { clear = true }),
         pattern = 'gruvbox',
