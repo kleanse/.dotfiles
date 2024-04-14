ffc3b33a448025e1995cc69d2a27fded5f287b11
Kenny Lam <kenny@laminosity.com>
2024-04-14 12:40:34 -0400

    Use nvim api function to set highlight groups
    Also specify the fg, bg, and sp attributes first so that their values
    can be quickly associated with their highlight group names.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -13,13 +13,13 @@ return {
           GruvboxRedUnderline    = { fg='#9d0006' },
           GruvboxYellowUnderline = { fg='#b57614' },
 
-          GruvboxAquaUnderlineItalic   = { undercurl=true, italic=true, fg='#427b58', sp='#427b58' },
-          GruvboxBlueUnderlineItalic   = { undercurl=true, italic=true, fg='#076678', sp='#076678' },
-          GruvboxGreenUnderlineItalic  = { undercurl=true, italic=true, fg='#79740e', sp='#79740e' },
-          GruvboxOrangeUnderlineItalic = { undercurl=true, italic=true, fg='#af3a03', sp='#af3a03' },
-          GruvboxPurpleUnderlineItalic = { undercurl=true, italic=true, fg='#8f3f71', sp='#8f3f71' },
-          GruvboxRedUnderlineItalic    = { undercurl=true, italic=true, fg='#9d0006', sp='#9d0006' },
-          GruvboxYellowUnderlineItalic = { undercurl=true, italic=true, fg='#b57614', sp='#b57614' },
+          GruvboxAquaUnderlineItalic   = { fg='#427b58', sp='#427b58', undercurl=true, italic=true },
+          GruvboxBlueUnderlineItalic   = { fg='#076678', sp='#076678', undercurl=true, italic=true },
+          GruvboxGreenUnderlineItalic  = { fg='#79740e', sp='#79740e', undercurl=true, italic=true },
+          GruvboxOrangeUnderlineItalic = { fg='#af3a03', sp='#af3a03', undercurl=true, italic=true },
+          GruvboxPurpleUnderlineItalic = { fg='#8f3f71', sp='#8f3f71', undercurl=true, italic=true },
+          GruvboxRedUnderlineItalic    = { fg='#9d0006', sp='#9d0006', undercurl=true, italic=true },
+          GruvboxYellowUnderlineItalic = { fg='#b57614', sp='#b57614', undercurl=true, italic=true },
 
           SpellBad   = { link='GruvboxRedUnderlineItalic' },
           SpellCap   = { link='GruvboxBlueUnderlineItalic' },
@@ -33,12 +33,12 @@ return {
         pattern = 'gruvbox',
         callback = function()
           -- mini.statusline highlight groups
-          vim.cmd.highlight('MiniStatuslineModeNormal  gui=bold guifg=#fbf1c7 guibg=#928374')
-          vim.cmd.highlight('MiniStatuslineModeInsert  gui=bold guifg=#fbf1c7 guibg=#076678')
-          vim.cmd.highlight('MiniStatuslineModeVisual  gui=bold guifg=#fbf1c7 guibg=#8f3f71')
-          vim.cmd.highlight('MiniStatuslineModeReplace gui=bold guifg=#fbf1c7 guibg=#9d0006')
-          vim.cmd.highlight('MiniStatuslineModeCommand gui=bold guifg=#fbf1c7 guibg=#b57614')
-          vim.cmd.highlight('MiniStatuslineModeOther   gui=bold guifg=#fbf1c7 guibg=#79740e')
+          vim.api.nvim_set_hl(0, 'MiniStatuslineModeNormal',  { fg='bg', bg='#928374', bold=true })
+          vim.api.nvim_set_hl(0, 'MiniStatuslineModeInsert',  { fg='bg', bg='#076678', bold=true })
+          vim.api.nvim_set_hl(0, 'MiniStatuslineModeVisual',  { fg='bg', bg='#8f3f71', bold=true })
+          vim.api.nvim_set_hl(0, 'MiniStatuslineModeReplace', { fg='bg', bg='#9d0006', bold=true })
+          vim.api.nvim_set_hl(0, 'MiniStatuslineModeCommand', { fg='bg', bg='#b57614', bold=true })
+          vim.api.nvim_set_hl(0, 'MiniStatuslineModeOther',   { fg='bg', bg='#79740e', bold=true })
         end,
       })
 
