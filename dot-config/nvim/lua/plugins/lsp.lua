59781b567f7fe48432614f7d62d0d074177ab6af
Kenny Lam <kenny@laminosity.com>
2024-10-19 16:44:13 -0400

    Specify hrsh7th/cmp-nvim-lsp as dependency
    This commit defines hrsh7th/cmp-nvim-lsp as a dependency of
    neovim/nvim-lspconfig because the latter uses the former in its
    configuration.

--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -28,6 +28,9 @@ return {
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { "j-hui/fidget.nvim", opts = {} },
+
+      -- Allows extra capabilities provided by nvim-cmp
+      "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
       -- [[ Configure LSP ]]
