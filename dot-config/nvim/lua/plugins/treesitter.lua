250d4511add94039f9dc2fab77c5f02e52328c2e
Kenny Lam <kenny@laminosity.com>
2024-10-22 14:27:02 -0400

    Revise nvim-treesitter/nvim-treesitter plugin spec
    Like Commit 1cee3d5 (Revise stevearc/conform.nvim plugin specification,
    2024-10-21), drop the explicit definition for the config property and
    set the plugin's options in opts to conform with the recommended way of
    configuring plugins in folke/lazy.nvim.

--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -2,28 +2,26 @@ return {
   { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
+    main = "nvim-treesitter.configs",
+    opts = {
       -- [[ Configure Treesitter ]]
       --  See `:help nvim-treesitter`
-      ---@diagnostic disable-next-line: missing-fields
-      require("nvim-treesitter.configs").setup({
-        -- Add languages to be installed here that you want installed for
-        -- treesitter
-        ensure_installed = { "bash", "c", "cpp", "lua", "markdown", "vim", "vimdoc" },
-        auto_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
+      -- Add languages to be installed here that you want installed for
+      -- treesitter
+      ensure_installed = { "bash", "c", "cpp", "lua", "markdown", "vim", "vimdoc" },
+      auto_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
 
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<c-space>",
-            node_incremental = "<c-space>",
-            scope_incremental = "<c-s>",
-            node_decremental = "<M-space>",
-          },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<c-space>",
+          node_incremental = "<c-space>",
+          scope_incremental = "<c-s>",
+          node_decremental = "<M-space>",
         },
-      })
-    end,
+      },
+    },
   },
 }
