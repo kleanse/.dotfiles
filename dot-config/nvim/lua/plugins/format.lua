1cee3d5db2f42ac63ecf8e0704eb1d015cefe972
Kenny Lam <kenny@laminosity.com>
2024-10-21 21:01:04 -0400

    Revise stevearc/conform.nvim plugin specification
    Specifically, add properties to lazy-load stevearc/conform.nvim, drop
    the explicit definition for the config property, and set the plugin's
    options in opts.

--- dot-config/nvim/lua/plugins/format.lua
+++ dot-config/nvim/lua/plugins/format.lua
@@ -1,46 +1,51 @@
 return {
   { -- Autoformat
     "stevearc/conform.nvim",
-    config = function()
-      vim.g.format_on_save = true
-
-      require("conform").setup({
-        formatters_by_ft = {
-          lua = { "stylua" },
-          -- Conform will run multiple formatters sequentially
-          python = { "isort", "black" },
-          -- Conform will run the first available formatter
-          javascript = { "prettierd", "prettier", stop_after_first = true },
-          tex = { "latexindent" },
-        },
-        format_on_save = function(bufnr)
-          -- Disable autoformat on certain filetypes
-          local ignore_filetypes = { "c", "cpp" }
-          if vim.g.format_on_save and not vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-            return { timeout_ms = 500, lsp_format = "fallback" }
-          end
+    event = "BufWritePre",
+    cmd = "ConformInfo",
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_format = "fallback" })
         end,
-        -- Custom formatters and changes to built-in formatters
-        formatters = {
-          latexindent = {
-            args = { "-m" },
-          },
-        },
-      })
-
-      local nmap = function(keys, func, desc)
-        vim.keymap.set("n", keys, func, { desc = desc })
-      end
-
-      nmap("<leader>f", function()
-        require("conform").format({ async = true, lsp_format = "fallback" })
-      end, "[F]ormat buffer")
-
-      nmap("<leader>tf", function()
-        vim.g.format_on_save = not vim.g.format_on_save
-        local prefix = vim.g.format_on_save and string.rep(" ", 2) or "no"
-        vim.api.nvim_echo({ { prefix .. "format" } }, false, {})
-      end, "[T]oggle [F]ormat on save")
+        desc = "[F]ormat buffer",
+      },
+      {
+        "<leader>tf",
+        function()
+          vim.g.format_on_save = not vim.g.format_on_save
+          local prefix = vim.g.format_on_save and string.rep(" ", 2) or "no"
+          vim.api.nvim_echo({ { prefix .. "format" } }, false, {})
+        end,
+        desc = "[T]oggle [F]ormat on save",
+      },
+    },
+    init = function()
+      vim.g.format_on_save = true
     end,
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        python = { "isort", "black" },
+        -- Conform will run the first available formatter
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        tex = { "latexindent" },
+      },
+      format_on_save = function(bufnr)
+        -- Disable autoformat on certain filetypes
+        local ignore_filetypes = { "c", "cpp" }
+        if vim.g.format_on_save and not vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return { timeout_ms = 500, lsp_format = "fallback" }
+        end
+      end,
+      -- Custom formatters and changes to built-in formatters
+      formatters = {
+        latexindent = {
+          args = { "-m" },
+        },
+      },
+    },
   },
 }
