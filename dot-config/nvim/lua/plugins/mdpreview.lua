b753ba8750e83a0de2dc39eeca6eaab9a91ac791
Kenny Lam <kenny@laminosity.com>
2024-10-28 12:43:03 -0400

    Add iamcco/markdown-preview.nvim plugin

---
+++ dot-config/nvim/lua/plugins/mdpreview.lua
@@ -0,0 +1,18 @@
+return {
+  { -- Preview Markdown files in your browser
+    "iamcco/markdown-preview.nvim",
+    build = function()
+      -- Load the plugin first to use the subsequent autoload function
+      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>tp",
+        "<Plug>MarkdownPreviewToggle",
+        ft = "markdown",
+        desc = "MarkdownPreview: [T]oggle [P]review",
+      },
+    },
+  },
+}
