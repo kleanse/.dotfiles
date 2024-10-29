7d4b8eab892125d3eb04c68e026d3c10092a14e8
Kenny Lam <kenny@laminosity.com>
2024-10-28 21:12:16 -0400

    Revise ellisonleao/gruvbox.nvim plugin spec
    Specifically, set the plugin's options in the opts table of the plugin
    specification for folke/lazy.nvim.

--- dot-config/nvim/lua/plugins/colorscheme.lua
+++ dot-config/nvim/lua/plugins/colorscheme.lua
@@ -2,31 +2,32 @@ return {
   { -- gruvbox color scheme
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        overrides = {
-          GruvboxAquaUnderline = { fg = "#427b58" },
-          GruvboxBlueUnderline = { fg = "#076678" },
-          GruvboxGreenUnderline = { fg = "#79740e" },
-          GruvboxOrangeUnderline = { fg = "#af3a03" },
-          GruvboxPurpleUnderline = { fg = "#8f3f71" },
-          GruvboxRedUnderline = { fg = "#9d0006" },
-          GruvboxYellowUnderline = { fg = "#b57614" },
+    opts = {
+      overrides = {
+        GruvboxAquaUnderline = { fg = "#427b58" },
+        GruvboxBlueUnderline = { fg = "#076678" },
+        GruvboxGreenUnderline = { fg = "#79740e" },
+        GruvboxOrangeUnderline = { fg = "#af3a03" },
+        GruvboxPurpleUnderline = { fg = "#8f3f71" },
+        GruvboxRedUnderline = { fg = "#9d0006" },
+        GruvboxYellowUnderline = { fg = "#b57614" },
 
-          GruvboxAquaUnderlineItalic = { fg = "#427b58", sp = "#427b58", undercurl = true, italic = true },
-          GruvboxBlueUnderlineItalic = { fg = "#076678", sp = "#076678", undercurl = true, italic = true },
-          GruvboxGreenUnderlineItalic = { fg = "#79740e", sp = "#79740e", undercurl = true, italic = true },
-          GruvboxOrangeUnderlineItalic = { fg = "#af3a03", sp = "#af3a03", undercurl = true, italic = true },
-          GruvboxPurpleUnderlineItalic = { fg = "#8f3f71", sp = "#8f3f71", undercurl = true, italic = true },
-          GruvboxRedUnderlineItalic = { fg = "#9d0006", sp = "#9d0006", undercurl = true, italic = true },
-          GruvboxYellowUnderlineItalic = { fg = "#b57614", sp = "#b57614", undercurl = true, italic = true },
+        GruvboxAquaUnderlineItalic = { fg = "#427b58", sp = "#427b58", undercurl = true, italic = true },
+        GruvboxBlueUnderlineItalic = { fg = "#076678", sp = "#076678", undercurl = true, italic = true },
+        GruvboxGreenUnderlineItalic = { fg = "#79740e", sp = "#79740e", undercurl = true, italic = true },
+        GruvboxOrangeUnderlineItalic = { fg = "#af3a03", sp = "#af3a03", undercurl = true, italic = true },
+        GruvboxPurpleUnderlineItalic = { fg = "#8f3f71", sp = "#8f3f71", undercurl = true, italic = true },
+        GruvboxRedUnderlineItalic = { fg = "#9d0006", sp = "#9d0006", undercurl = true, italic = true },
+        GruvboxYellowUnderlineItalic = { fg = "#b57614", sp = "#b57614", undercurl = true, italic = true },
 
-          SpellBad = { link = "GruvboxRedUnderlineItalic" },
-          SpellCap = { link = "GruvboxBlueUnderlineItalic" },
-          SpellRare = { link = "GruvboxPurpleUnderlineItalic" },
-          SpellLocal = { link = "GruvboxAquaUnderlineItalic" },
-        },
-      })
+        SpellBad = { link = "GruvboxRedUnderlineItalic" },
+        SpellCap = { link = "GruvboxBlueUnderlineItalic" },
+        SpellRare = { link = "GruvboxPurpleUnderlineItalic" },
+        SpellLocal = { link = "GruvboxAquaUnderlineItalic" },
+      },
+    },
+    config = function(_, opts)
+      require("gruvbox").setup(opts)
 
       vim.api.nvim_create_autocmd("ColorScheme", {
         group = vim.api.nvim_create_augroup("highlight-colorscheme", { clear = true }),
