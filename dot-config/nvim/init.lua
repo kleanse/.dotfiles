cf580213b17265d6eae5ebc0fa80fdc9a0b85f98
Kenny Lam <kenny@laminosity.com>
2024-10-31 14:47:48 -0400

    Refactor Tabline() to use Nvim API functions

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -188,41 +188,41 @@ function Tabline()
   local s = ""
   local col = 0
   local columns = vim.api.nvim_get_option_value("columns", {})
-  local tabcount = vim.fn.tabpagenr("$")
+  local tabpages = vim.api.nvim_list_tabpages()
 
-  for i = 1, tabcount do
+  for tabnr, tabpage in ipairs(tabpages) do
     if col >= columns - 4 then
       break
     end
 
     local startcol = col
 
-    local bufnrlist = vim.fn.tabpagebuflist(i)
-    local winnr = vim.fn.tabpagewinnr(i)
-    local bufname = vim.fn.bufname(bufnrlist[winnr])
+    local bufnrlist = vim.fn.tabpagebuflist(tabnr)
+    local winnr = vim.api.nvim_win_get_number(vim.api.nvim_tabpage_get_win(tabpage))
+    local bufname = vim.api.nvim_buf_get_name(bufnrlist[winnr])
 
     -- Highlight current tab page
     local hlgroup = "%#TabLine#"
-    if i == vim.fn.tabpagenr() then
+    if tabpage == vim.api.nvim_get_current_tabpage() then
       hlgroup = "%#TabLineSel#"
     end
     s = s .. hlgroup
 
     -- Set tab page number (for mouse clicks)
-    s = s .. "%" .. i .. "T"
+    s = s .. "%" .. tabnr .. "T"
 
     s = s .. " "
     col = col + 1
 
     local modified = false
     for _, bufnr in ipairs(bufnrlist) do
-      if vim.fn.getbufvar(bufnr, "&modified") == 1 then
+      if vim.api.nvim_get_option_value("modified", { buf = bufnr }) then
         modified = true
         break
       end
     end
 
-    local wincount = vim.fn.tabpagewinnr(i, "$")
+    local wincount = #vim.api.nvim_tabpage_list_wins(tabpage)
     if modified or wincount > 1 then
       if wincount > 1 then
         local len = #string.format("%d", wincount)
@@ -247,7 +247,7 @@ function Tabline()
     end
 
     -- Set tab page label
-    local tabwidth = math.max(tabcount > 0 and math.floor((columns - 1 + tabcount / 2) / tabcount) or 0, 6)
+    local tabwidth = math.max(#tabpages > 0 and math.floor((columns - 1 + #tabpages / 2) / #tabpages) or 0, 6)
     local room = startcol - col + tabwidth - 1
     if room > 0 then
       local trbufname = bufname ~= "" and vim.fn.fnamemodify(bufname, ":t") or "[No Name]"
@@ -267,15 +267,15 @@ function Tabline()
   local showcmd = vim.api.nvim_get_option_value("showcmd", {})
   local showcmdloc = vim.api.nvim_get_option_value("showcmdloc", {})
   if showcmd and showcmdloc == "tabline" then
-    local sc_width = math.min(10, columns - col - (tabcount > 1 and 3 or 0))
+    local sc_width = math.min(10, columns - col - (#tabpages > 1 and 3 or 0))
     if sc_width > 0 then
-      s = s .. string.rep(" ", columns - sc_width - (tabcount > 1 and 2 or 0) - col)
+      s = s .. string.rep(" ", columns - sc_width - (#tabpages > 1 and 2 or 0) - col)
       s = s .. "%#TabLine#%S%#TabLineFill#"
     end
   end
 
   -- Right-align the label to close the current tab page
-  if tabcount > 1 then
+  if #tabpages > 1 then
     s = s .. "%=%#TabLine#%999XX"
   end
 
