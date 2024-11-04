c0f39a125fd760b7d84d8be12da2487283fa8c40
Kenny Lam <kenny@laminosity.com>
2024-11-04 09:40:45 -0500

    Ensure prettierd formatter is installed

--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -117,6 +117,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         "markdown-toc",
+        "prettierd",
         "stylua", -- Used to format lua code
       })
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
