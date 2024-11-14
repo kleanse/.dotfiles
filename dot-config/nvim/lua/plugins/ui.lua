2589b552c35db5ec050a83ba407ae95bd65a103d
Kenny Lam <kenny@laminosity.com>
2024-11-14 13:22:03 -0500

    Add diff section to mini.statusline

--- dot-config/nvim/lua/plugins/ui.lua
+++ dot-config/nvim/lua/plugins/ui.lua
@@ -39,7 +39,8 @@ return {
         active = function()
           local MiniStatusline = require("mini.statusline")
           local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-          local git = MiniStatusline.section_git({ trunc_width = 75 })
+          local git = MiniStatusline.section_git({ trunc_width = 40 })
+          local diff = MiniStatusline.section_diff({ trunc_width = 75 })
           local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
           local filename = MiniStatusline.section_filename({ trunc_width = 140 })
           local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
@@ -49,7 +50,7 @@ return {
 
           return MiniStatusline.combine_groups({
             { hl = mode_hl, strings = { mode } },
-            { hl = "MiniStatuslineDevinfo", strings = { git, diagnostics } },
+            { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics } },
             "%<", -- Mark general truncate point
             { hl = "MiniStatuslineFilename", strings = { filename } },
             "%=", -- End left alignment
