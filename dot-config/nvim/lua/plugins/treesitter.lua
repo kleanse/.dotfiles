7a7c7764327aaf6b75793ff46f1407d47f9045e5
Kenny Lam <kenny@laminosity.com>
2024-11-16 10:32:23 -0500

    Add event to nvim-treesitter/nvim-treesitter spec

--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
+    event = "VeryLazy",
     opts = {
       -- [[ Configure Treesitter ]]
       --  See `:help nvim-treesitter`
