706fa6d624862d46af4f099b355f74cd1e9b9345
Kenny Lam <kenny@laminosity.com>
2024-11-17 10:50:00 -0500

    Lazy-load tpope/vim-fugitive plugin spec
    By defining mappings in the keys field and specifying cmd.

--- dot-config/nvim/lua/plugins/git.lua
+++ dot-config/nvim/lua/plugins/git.lua
@@ -25,47 +25,42 @@ return {
     dependencies = {
       "tpope/vim-rhubarb",
     },
+    cmd = "Git",
+    keys = {
+      {
+        "<Leader>G",
+        function()
+          local window_ids = vim.fn.win_findbuf(vim.g.git_status_bufnr)
+          if #window_ids ~= 0 then
+            vim.fn.win_gotoid(window_ids[1])
+            vim.cmd.edit()
+          else
+            vim.cmd("0tab Git")
+            vim.g.git_status_bufnr = vim.fn.bufnr()
+          end
+        end,
+        desc = "Fugitive: Git status",
+      },
+      -- stylua: ignore start
+      { "<Leader>gbl", function() vim.cmd("Git blame --date=relative") end, desc = "Fugitive: Git blame" },
+      { "<Leader>glg", function() vim.cmd("Git log --stat") end, desc = "Fugitive: Git log --stat" },
+      { "<Leader>glo", function() vim.cmd("Git log --oneline --decorate") end, desc = "Fugitive: Git log --oneline" },
+      -- stylua: ignore end
+      { -- Pretty print the relative author dates and author names alongside
+        -- commits
+        --  See `:Man git-log` under format:<format-string> in the "PRETTY
+        --  FORMATS" section for details about these placeholders
+        "<Leader>glp",
+        function()
+          vim.cmd(
+            'Git log --graph --pretty="%Cred%h%Creset%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset"'
+          )
+        end,
+        desc = "Fugitive: log --pretty",
+      },
+    },
     init = function()
       vim.g.git_status_bufnr = -1
-    end,
-    config = function()
-      -- [[ Configure Fugitive ]]
-      --  See `:help fugitive`
-      local nmap = function(keys, func, desc)
-        desc = desc and "Git " .. desc
-        vim.keymap.set("n", keys, func, { desc = desc })
-      end
-
-      nmap("<Leader>glg", function()
-        vim.cmd("Git log --stat")
-      end, "log --stat")
-
-      nmap("<Leader>glo", function()
-        vim.cmd("Git log --oneline --decorate")
-      end, "log --oneline")
-
-      nmap("<Leader>gbl", function()
-        vim.cmd("Git blame --date=relative")
-      end, "blame --date=relative")
-
-      nmap("<Leader>G", function()
-        local window_ids = vim.fn.win_findbuf(vim.g.git_status_bufnr)
-        if #window_ids ~= 0 then
-          vim.fn.win_gotoid(window_ids[1])
-          vim.cmd.edit()
-        else
-          vim.cmd("0tab Git")
-          vim.g.git_status_bufnr = vim.fn.bufnr()
-        end
-      end, "status")
-
-      -- Pretty print the relative author dates and author names alongside
-      -- commits
-      --  See `:Man git-log` under format:<format-string> in the "PRETTY
-      --  FORMATS" section for details about these placeholders
-      nmap("<Leader>glp", function()
-        vim.cmd('Git log --graph --pretty="%Cred%h%Creset%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset"')
-      end, "log --pretty")
     end,
   },
 }
