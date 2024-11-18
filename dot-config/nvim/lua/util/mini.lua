f0074820fc9857430864a8ba62344f342f3e417e
Kenny Lam <kenny@laminosity.com>
2024-11-18 10:33:08 -0500

    Make mini.pairs code block more restrictive
    Specifically, complete the Markdown code block only if the line contains
    any leading whitespace and only two backticks at the end of line.

--- dot-config/nvim/lua/util/mini.lua
+++ dot-config/nvim/lua/util/mini.lua
@@ -16,8 +16,7 @@ function M.pairs(opts)
     local line = vim.api.nvim_get_current_line()
     local cursor = vim.api.nvim_win_get_cursor(0)
     local next = line:sub(cursor[2] + 1, cursor[2] + 1)
-    local before = line:sub(1, cursor[2])
-    if opts.markdown and o == "`" and vim.bo.filetype == "markdown" and before:match("^%s*``") then
+    if opts.markdown and o == "`" and vim.bo.filetype == "markdown" and line:match("^%s*``$") then
       return "`\n```" .. vim.api.nvim_replace_termcodes("<Up>", true, true, true)
     end
     if opts.skip_next and next ~= "" and next:match(opts.skip_next) then
