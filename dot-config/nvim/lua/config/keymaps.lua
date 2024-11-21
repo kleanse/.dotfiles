50d2a31269355c1dd4b77cecdff216a384e0982d
Kenny Lam <kenny@laminosity.com>
2024-11-21 18:04:20 -0500

    Revise arrangement of mappings in keymaps.lua
    Specifically, group mappings by mode and mapping type and sort them
    lexically in their groups by their left-hand sides.

--- dot-config/nvim/lua/config/keymaps.lua
+++ dot-config/nvim/lua/config/keymaps.lua
@@ -8,20 +8,9 @@ map("", "+", ";")
 map("", "-", ",")
 
 -- Remap for dealing with word wrap
-map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 -- Move tabs
-map("n", "gy", function()
-  if vim.v.count ~= 0 then
-    vim.cmd.tabmove(vim.v.count)
-  elseif vim.api.nvim_tabpage_get_number(0) == 1 then
-    vim.cmd.tabmove("$")
-  else
-    vim.cmd.tabmove("-")
-  end
-end, { desc = "Move the current tab page to the left" })
-
 map("n", "gl", function()
   if vim.v.count ~= 0 then
     vim.cmd.tabmove(vim.v.count)
@@ -31,46 +20,43 @@ map("n", "gl", function()
     vim.cmd.tabmove("+")
   end
 end, { desc = "Move the current tab page to the right" })
-
--- Reference local ASCII table
-map("n", "<Leader>va", "<Cmd>split $ASCII_REFERENCE | view<CR>", { desc = "View ASCII reference file in a new split" })
-
+map("n", "gy", function()
+  if vim.v.count ~= 0 then
+    vim.cmd.tabmove(vim.v.count)
+  elseif vim.api.nvim_tabpage_get_number(0) == 1 then
+    vim.cmd.tabmove("$")
+  else
+    vim.cmd.tabmove("-")
+  end
+end, { desc = "Move the current tab page to the left" })
 -- Keep cursor centered when repeating searches, opening any closed folds if
 -- necessary
-map("n", "n", "nzvzz")
 map("n", "N", "Nzvzz")
-
-map("n", "<Leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic quickfix list" })
-jump_map("q", { next = vim.cmd.cnext, prev = vim.cmd.cprevious }, { name = "error" })
-
+map("n", "n", "nzvzz")
 map("n", ";", vim.cmd.update, { desc = '":update" file' })
-map("n", "<M-b>", "<C-^>")
-map("n", "<M-e>", vim.cmd.Explore, { desc = "Netrw explore directory of current file" })
-map("n", "<M-o>", vim.cmd.Rexplore, { desc = "Netrw return to or from Explorer" })
-
-map("n", "<M-g>", vim.cmd.pwd, { desc = "Print current working directory of current window" })
-
-map("n", "<Leader>l", vim.cmd.Lazy, { desc = "Open Lazy" })
-
 map("n", "<Leader>OT", function()
   vim.cmd.only()
   vim.cmd.tabonly()
 end, { desc = "Only This window" })
-
+jump_map("q", { next = vim.cmd.cnext, prev = vim.cmd.cprevious }, { name = "error" })
+map("n", "<Leader>l", vim.cmd.Lazy, { desc = "Open Lazy" })
 map("n", "<leader>n", vim.cmd.enew, { desc = "Edit new buffer" })
+map("n", "<Leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic quickfix list" })
+map("n", "<Leader>va", "<Cmd>split $ASCII_REFERENCE | view<CR>", { desc = "View ASCII reference file in a new split" })
+map("n", "<M-b>", "<C-^>") -- Alternate buffer
+map("n", "<M-e>", vim.cmd.Explore, { desc = "Netrw explore directory of current file" })
+map("n", "<M-g>", vim.cmd.pwd, { desc = "Print current working directory of current window" })
+map("n", "<M-o>", vim.cmd.Rexplore, { desc = "Netrw return to or from Explorer" })
 
 -- Use CTRL-C for <Esc>: it is easier to reach
 map("i", "<C-C>", "<Esc>")
 map("x", "<C-C>", "<Esc>")
-
-map("i", "<C-L>", "<C-X><C-L>")
-
+map("i", "<C-L>", "<C-X><C-L>") -- Complete whole lines
 -- Move text in Visual mode. Visual-mode "J" and "K" are overwritten; for the
 -- former command, use ":join" instead. The latter might not need addressed:
 -- Visual-mode "K" is rare
 map("x", "J", ":move '>+1<CR>gv=gv", { desc = "Move selected text down one line" })
 map("x", "K", ":move '<-2<CR>gv=gv", { desc = "Move selected text up one line" })
-
 -- Use some common GNU-Readline keyboard shortcuts for the Command line
 -- Overwrite the Command-line commands CTRL-A, CTRL-B, and CTRL-F. CTRL-A is
 -- not useful; CTRL-B's behavior is moved to CTRL-A; and CTRL-F expedites
@@ -80,7 +66,6 @@ map("c", "<C-B>", "<Left>")
 map("c", "<C-F>", "<Right>")
 map("c", "<M-b>", "<S-Left>")
 map("c", "<M-f>", "<S-Right>")
-
 -- Overload Command-line CTRL-D: it still performs its normal behavior, but if
 -- a character is under the cursor, it executes the GNU-Readline behavior
 -- (i.e., delete the character).
