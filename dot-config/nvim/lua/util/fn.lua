d3b7dbf882e2e22f987bc0d409739c2e1c94affc
Kenny Lam <kenny@laminosity.com>
2024-11-21 14:49:52 -0500

    Add functions to delete buffers but keep windows

--- dot-config/nvim/lua/util/fn.lua
+++ dot-config/nvim/lua/util/fn.lua
@@ -3,6 +3,98 @@
 ---@class util.fn
 local M = {}
 
+---@class util.fn.bufdelete.Opts
+---@field force? boolean Delete the buffer even if it is modified
+---@field filter? fun(buf: number): boolean Filter for buffers to delete
+---@field wipe? boolean Wipe the buffer instead of deleting it (see `:h :bwipeout`)
+
+-- Function obtained from folke/snacks.nvim
+
+--- Deletes the current buffer if `{buffer}` is `nil`, the buffer whose handle
+--- is `{buffer}`, or every buffer from which the predicate `{opts.filter}`
+--- returns `true`, if specified. This function is similar to
+--- |nvim_buf_delete()| but keeps windows open.
+---@param buffer? number
+---@param opts? number|util.fn.bufdelete.Opts
+function M.bufdelete(buffer, opts)
+  opts = opts or {}
+  opts = type(opts) == "function" and { filter = opts } or opts
+  ---@cast opts util.fn.bufdelete.Opts
+  if type(opts.filter) == "function" then
+    for _, buf in ipairs(vim.tbl_filter(opts.filter, vim.api.nvim_list_bufs())) do
+      if vim.bo[buf].buflisted then
+        M.bufdelete(buf, vim.tbl_extend("force", {}, opts, { filter = false }))
+      end
+    end
+    return
+  end
+  local buf = buffer or 0
+  buf = buf == 0 and vim.api.nvim_get_current_buf() or buf
+  vim.api.nvim_buf_call(buf, function()
+    if vim.bo.modified and not opts.force then
+      local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+      if choice == 0 or choice == 3 then -- 0 for <Esc> or <C-C> and 3 for Cancel
+        return
+      end
+      if choice == 1 then -- Yes
+        vim.cmd.write()
+      end
+    end
+    -- Keep the windows viewing the buffer to be deleted open by populating
+    -- them with a different buffer
+    for _, win in ipairs(vim.fn.win_findbuf(buf)) do
+      vim.api.nvim_win_call(win, function()
+        if not vim.api.nvim_win_is_valid(win) or vim.api.nvim_win_get_buf(win) ~= buf then
+          return
+        end
+        -- Try using alternate buffer
+        local alt = vim.fn.bufnr("#")
+        if alt ~= buf and vim.fn.buflisted(alt) == 1 then
+          vim.api.nvim_win_set_buf(win, alt)
+          return
+        end
+        -- Try using previous buffer
+        local has_previous = pcall(vim.cmd, "bprevious")
+        if has_previous and buf ~= vim.api.nvim_win_get_buf(win) then
+          return
+        end
+        -- Create new listed buffer
+        local new_buf = vim.api.nvim_create_buf(true, false)
+        vim.api.nvim_win_set_buf(win, new_buf)
+      end)
+    end
+    if vim.api.nvim_buf_is_valid(buf) then
+      pcall(vim.cmd, (opts.wipe and "bwipeout! " or "bdelete! ") .. buf)
+    end
+  end)
+end
+
+--- Deletes all buffers.
+---@param opts? util.fn.bufdelete.Opts
+function M.bufdeleteall(opts)
+  return M.bufdelete(
+    nil,
+    vim.tbl_extend("force", {}, opts or {}, {
+      filter = function()
+        return true
+      end,
+    })
+  )
+end
+
+--- Deletes all buffers except the current one.
+---@param opts? util.fn.bufdelete.Opts
+function M.bufonly(opts)
+  return M.bufdelete(
+    nil,
+    vim.tbl_extend("force", {}, opts or {}, {
+      filter = function(b)
+        return b ~= vim.api.nvim_get_current_buf()
+      end,
+    })
+  )
+end
+
 --- Returns an array of symbol kinds with which to filter the results of LSP
 --- document symbols listed in the builtin pickers of
 --- `nvim-telescope/telescope.nvim`. The filter array to return is based on the
