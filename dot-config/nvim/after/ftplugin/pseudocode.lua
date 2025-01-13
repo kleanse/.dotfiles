d5d220abc2cc4698433248b5737618220442f0bf
Kenny Lam <kenny@laminosity.com>
2025-01-13 12:29:15 -0500

    Disable auto-wrap text for Pseudocode files

--- dot-config/nvim/after/ftplugin/pseudocode.lua
+++ dot-config/nvim/after/ftplugin/pseudocode.lua
@@ -1,8 +1,9 @@
 -- Language:	Pseudocode
--- Last Change:	2024 Dec 05
+-- Last Change:	2025 Jan 13
 
 vim.b.undo_ftplugin = "setl fo<"
 
 local setl = vim.opt_local
 
-setl.formatoptions:append("tcroqlj")
+setl.formatoptions:remove("t")
+setl.formatoptions:append("croqlj")
